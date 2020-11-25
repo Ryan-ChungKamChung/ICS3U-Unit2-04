41c56cff2319507cfec6fd4fdddd8b9f9938f64b
Ubuntu <ubuntu@ip-172-31-54-182.ec2.internal>
2020-11-25 18:05:28 +0000

    added constants.py and pizza_cost.py

---
+++ constants.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Created by Ryan Chung Kam Chung
+# Created in November 2020
+# Program defining constants for pizza_cost.py
+
+RENT = 1
+LABOUR = 0.75
+COST_PER_INCH = 0.5
+HST = 0.13
